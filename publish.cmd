1a5e72b61f2aa704ea61c4960421e5ef3dffd1e3
deshrajsrai <singhdeshraj73@gmail.com>
2018-08-01 12:39:54 +0530

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=bechtelmyaccessbot-Web-Deploy.pubxml -p:Password=T9KHHrbzxiW1ucWbvtc1ZdgNSHbzYclojlEgmhHwRnl0xwlWsqM43lFflufi
+
